1117b10e13fb25263b6b1bcc387dab294c470564
Eduardo Damian Dantas <dantas@e3c.com.br>
2015-07-28 15:25:29 -0300

    Add project files

---
+++ src/net/signagewidgets/serial/services/SerialService.java
@@ -0,0 +1,82 @@
+package net.signagewidgets.serial.services;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+
+import net.signagewidgets.serial.services.SerialDeviceManager.SerialDeviceListener;
+import net.signagewidgets.serial.util.Logging;
+
+public class SerialService extends Service implements SerialDeviceListener {
+	private static Logging sLogging = new Logging(SerialService.class);	
+
+	private static final String START = "net.signagewidgets.serial.services.SerialService.START";
+	private static final String STOP = "net.signagewidgets.serial.services.SerialService.STOP";
+	private static final String ID = "DEVICE_ID";
+
+	private SerialDeviceManager mDeviceManager;
+
+	public static void start(Context context) {
+		Intent intent = new Intent(context, SerialService.class);
+		intent.setAction(START);
+		context.startService(intent);
+	}
+
+	public static void stop(Context context, int deviceId) {
+		Intent intent = new Intent(context, SerialService.class);
+		intent.setAction(STOP);
+		intent.putExtra(ID, deviceId);
+		context.startService(intent);
+	}
+
+	@Override
+	public void onCreate() {
+		sLogging.info("onCreate");
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		if (START.equals(intent.getAction())) {
+			sLogging.info("Start command received");
+			if (mDeviceManager == null) mDeviceManager = new SerialDeviceManager(this, this);
+		} else if (STOP.equals(intent.getAction())) {
+			sLogging.info("Stop command received");
+			if (mDeviceManager != null
+					&& intent.getIntExtra(ID, -1) == mDeviceManager.getDeviceID()) {
+				mDeviceManager.destroy();
+				mDeviceManager = null;
+				android.os.Process.killProcess(android.os.Process.myPid());
+			}
+		}
+
+		return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy() {
+		sLogging.info("Destroy");
+		super.onDestroy();
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onLineReceived(String line) {
+		try {
+    		long id = Integer.valueOf(line.substring(0, line.length()-1));
+    		long button = Integer.valueOf(line.substring(line.length()-1));
+    		sLogging.info("ID:", id, "- Button:", button);
+    		Intent intent = new Intent("net.signagewidgets.serial.BUTTON");
+    		intent.putExtra("id", id);
+    		intent.putExtra("button", button);
+    		sendBroadcast(intent);
+		} catch (Exception e) {
+			sLogging.error("Error parsing serial data");
+			sLogging.captureException(e);
+		}
+	}
+}
---
+++ src/net/signagewidgets/serial/util/CaptureExceptionsScheduledExecutor.java
@@ -0,0 +1,147 @@
+
+package net.signagewidgets.serial.util;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+public class CaptureExceptionsScheduledExecutor extends ScheduledThreadPoolExecutor {
+    static private Logging sLogging = new Logging(CaptureExceptionsScheduledExecutor.class);
+
+    private static class NamedThreadFactory implements ThreadFactory {
+        private final String mName;
+
+        public NamedThreadFactory(String name) {
+            mName = name;
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+          return new Thread(r, mName);
+
+        }
+    }
+
+    public CaptureExceptionsScheduledExecutor(int corePoolSize, String name) {
+        super(corePoolSize, new NamedThreadFactory(name));
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        try {
+            super.execute(detectExceptionsIn(command));
+        } catch (RejectedExecutionException e) {
+            sLogging.warning("Job rejected, executor is shutdown.");
+        }
+    }
+
+    @Override
+    public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
+        try {
+            return super.schedule(detectExceptionsIn(command), delay, unit);
+        } catch (RejectedExecutionException e) {
+            sLogging.warning("Job rejected, executor is shutdown.");
+        }
+        return null;
+    }
+
+    @Override
+    public Future<?> submit(Runnable task) {
+        try {
+            return super.submit(detectExceptionsIn(task));
+        } catch (RejectedExecutionException e) {
+            sLogging.warning("Job rejected, executor is shutdown.");
+        }
+        return null;
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay,
+            long delay, TimeUnit unit) {
+        try {
+            return super.scheduleWithFixedDelay(detectExceptionsIn(command), initialDelay, delay, unit);
+        } catch (RejectedExecutionException e) {
+            sLogging.warning("Job rejected, executor is shutdown.");
+        }
+        return null;
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period,
+            TimeUnit unit) {
+        try {
+            return super.scheduleAtFixedRate(detectExceptionsIn(command), initialDelay, period, unit);
+        } catch (RejectedExecutionException e) {
+            sLogging.warning("Job rejected, executor is shutdown.");
+        }
+        return null;
+    }
+
+    @Override
+    public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
+        try {
+            return super.schedule(detectExceptionsIn(callable), delay, unit);
+        } catch (RejectedExecutionException e) {
+            sLogging.warning("Job rejected, executor is shutdown.");
+        }
+        return null;
+    }
+
+    @Override
+    public <T> Future<T> submit(Callable<T> task) {
+        try {
+            return super.submit(detectExceptionsIn(task));
+        } catch (RejectedExecutionException e) {
+            sLogging.warning("Job rejected, executor is shutdown.");
+        }
+        return null;
+    }
+
+    @Override
+    public <T extends Object> java.util.concurrent.Future<T> submit(Runnable task, T result) {
+        try {
+            return super.submit(detectExceptionsIn(task), result);
+        } catch (RejectedExecutionException e) {
+            sLogging.warning("Job rejected, executor is shutdown.");
+        }
+        return null;
+    };
+
+    private static Runnable detectExceptionsIn(final Runnable command) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    command.run();
+                } catch (Exception e) {
+                    onException(e);
+                }
+            }
+        };
+    }
+
+    private static <V> Callable<V> detectExceptionsIn(final Callable<V> callable) {
+        return new Callable<V>() {
+            @Override
+            public V call() throws Exception {
+                try {
+                    return callable.call();
+                } catch (Exception e) {
+                    onException(e);
+                }
+
+                return null;
+            }
+        };
+    }
+
+    private static void onException(Exception e) {
+        UncaughtExceptionHandler handler = Thread.getDefaultUncaughtExceptionHandler();
+        if (handler != null) handler.uncaughtException(Thread.currentThread(), e);
+    }
+}
