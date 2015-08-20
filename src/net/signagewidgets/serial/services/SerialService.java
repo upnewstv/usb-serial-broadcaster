2fd910cb0f1967d0910fff6bc2247c9f66d324be
Eduardo Damian Dantas <dantas@e3c.com.br>
2015-08-20 15:40:18 -0300

    Make the service foreground

--- src/net/signagewidgets/serial/services/SerialService.java
+++ src/net/signagewidgets/serial/services/SerialService.java
@@ -1,8 +1,10 @@
 package net.signagewidgets.serial.services;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
 
 import net.signagewidgets.serial.services.SerialDeviceManager.SerialDeviceListener;
@@ -14,6 +16,7 @@ public class SerialService extends Service implements SerialDeviceListener {
 	private static final String START = "net.signagewidgets.serial.services.SerialService.START";
 
 	private SerialDeviceManager mDeviceManager;
+	private Notification mNotification;
 
 	public static void start(Context context) {
 		Intent intent = new Intent(context, SerialService.class);
@@ -21,15 +24,33 @@ public class SerialService extends Service implements SerialDeviceListener {
 		context.startService(intent);
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate() {
 		sLogging.info("onCreate");
+
+		Notification.Builder builder = new Notification.Builder(SerialService.this)
+	        .setContentTitle(getApplicationInfo().loadLabel(getPackageManager()))
+	        .setSmallIcon(net.signagewidgets.serial.R.drawable.icon)
+	        .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+	        		net.signagewidgets.serial.R.drawable.icon))
+	        .setAutoCancel(false);
+
+        if (android.os.Build.VERSION.SDK_INT < 16) {
+            mNotification = builder.getNotification();
+        }  else {
+            mNotification = builder.build();
+        }
+
+        mNotification.flags |= Notification.FLAG_ONGOING_EVENT;
+        mNotification.flags |= Notification.FLAG_NO_CLEAR;
+        startForeground(5378, mNotification);
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		String action = intent != null ? intent.getAction() : null;
-		
+
 		if (START.equals(action)) {
 			sLogging.info("Start command received");
 			if (mDeviceManager == null) mDeviceManager = new SerialDeviceManager(this, this);
@@ -41,6 +62,10 @@ public class SerialService extends Service implements SerialDeviceListener {
 	@Override
 	public void onDestroy() {
 		sLogging.info("Destroy");
+		if (mDeviceManager != null) {
+			mDeviceManager.destroy();
+			mDeviceManager = null;
+		}
 		super.onDestroy();
 	}
 
