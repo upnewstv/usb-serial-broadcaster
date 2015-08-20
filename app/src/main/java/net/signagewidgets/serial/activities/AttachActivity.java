de64ea9269a6dbfe3641ca3dc92818af46a4853d
lenoir zamboni <lenoirzamboni@E3Cs-Mac-mini.local>
2015-08-20 14:30:38 -0300

    Added floatind action button - fab (add control). It's using the library 'com.android.support:design:23.0.0'. It has support to automatically add the fab (CustomView) without using the old method -> view + layout(shape).

--- app/src/main/java/net/signagewidgets/serial/activities/AttachActivity.java
+++ app/src/main/java/net/signagewidgets/serial/activities/AttachActivity.java
@@ -1,8 +1,15 @@
 package net.signagewidgets.serial.activities;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import net.signagewidgets.serial.R;
 import net.signagewidgets.serial.services.SerialService;
 
 public class AttachActivity extends Activity {
