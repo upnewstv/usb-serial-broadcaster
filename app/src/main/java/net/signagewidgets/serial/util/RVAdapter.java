81895cd65d579c56fd00e282f0d5c28b827d22ef
lenoir zamboni <lenoirzamboni@E3Cs-Mac-mini.e3c.lan>
2015-08-21 14:32:57 -0300

    Added method 'getItemViewType' in RVAdapter class. It will be responsible for set the specific layout for remote control (2 or 4 buttons)

--- app/src/main/java/net/signagewidgets/serial/util/RVAdapter.java
+++ app/src/main/java/net/signagewidgets/serial/util/RVAdapter.java
@@ -8,7 +8,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.signagewidgets.serial.R;
-
 import net.signagewidgets.serial.model.RemoteControl;
 
 /**
@@ -16,60 +15,81 @@ import net.signagewidgets.serial.model.RemoteControl;
  */
 
 public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
-        private RemoteControl [] remoteControls;
+    private RemoteControl [] remoteControls;
 
-        public RVAdapter(RemoteControl[] remoteControls) {
-            this.remoteControls = remoteControls;
-        }
+    public RVAdapter(RemoteControl[] remoteControls) {
+        this.remoteControls = remoteControls;
+    }
 
-        // For each item
+    // inner class to hold a reference to each item of RecyclerView
+    public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        // Inflate item_layout
-        // Create a ViewHolder
-        // Bind the view “ViewHolder” with data “remoteControls”
+        public TextView controlName;
+        public TextView dateAdd;
+        public ImageView iconControl;
 
-        // Create new views (invoked by the layout manager)
-        @Override
-        public RVAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-            // create a new view (decide which type of view should be created!!!)
-            View itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_control_2_buttons, null);
-
-            // create ViewHolder
-            ViewHolder viewHolder = new ViewHolder(itemLayoutView);
-            return viewHolder;
-        }
-
-        // Replace the contents of a view (invoked by the layout manager)
-        @Override
-        public void onBindViewHolder(ViewHolder viewHolder, int position) {
-
-            // - get data from your remoteControls at this position
-            // - replace the contents of the view with that remoteControls
-
-            viewHolder.controlName.setText(remoteControls[position].getName());
-            viewHolder.dateAdd.setText(remoteControls[position].getDate());
-            viewHolder.iconControl.setImageResource(remoteControls[position].getTypeIcon());
-        }
-
-        // inner class to hold a reference to each item of RecyclerView
-        public static class ViewHolder extends RecyclerView.ViewHolder {
-
-            public TextView controlName;
-            public TextView dateAdd;
-            public ImageView iconControl;
-
-            public ViewHolder(View itemLayoutView) {
-                super(itemLayoutView);
-                controlName = (TextView) itemLayoutView.findViewById(R.id.name_control);
-                dateAdd = (TextView) itemLayoutView.findViewById(R.id.date_add);
-                iconControl = (ImageView) itemLayoutView.findViewById(R.id.icon_control);
-            }
-        }
-
-        // Return the size of your remoteControls (invoked by the layout manager)
-        @Override
-        public int getItemCount() {
-            return remoteControls.length;
+        public ViewHolder(View itemLayoutView) {
+            super(itemLayoutView);
+            controlName = (TextView) itemLayoutView.findViewById(R.id.name_control);
+            dateAdd = (TextView) itemLayoutView.findViewById(R.id.date_add);
+            iconControl = (ImageView) itemLayoutView.findViewById(R.id.icon_control);
         }
     }
+
+    // For each item
+
+    // Inflate item_layout
+    // Create a ViewHolder
+    // Bind the view “ViewHolder” with data “remoteControls”
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public RVAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemLayoutView = null;
+
+        switch (viewType) {
+
+            case 2:
+                // create a new view
+                itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_control_2_buttons, null);
+                break;
+
+            default:
+                // create a new view
+                itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_control_4_buttons, null);
+                break;
+        }
+        // create ViewHolder
+        return new ViewHolder(itemLayoutView);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        if(remoteControls[position].getIdButtons().size() == 2){
+            return 2;
+        }else{
+            return 4;
+        }
+    }
+
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+
+        // - get data from your remoteControls at this position
+        // - replace the contents of the view with that remoteControls
+
+        viewHolder.controlName.setText(remoteControls[position].getName());
+        viewHolder.dateAdd.setText(remoteControls[position].getDate());
+        viewHolder.iconControl.setImageResource(remoteControls[position].getIdControl());
+    }
+
+    // Return the size of your remoteControls (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return remoteControls.length;
+    }
+}
